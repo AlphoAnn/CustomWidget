8396018d6cf0ffa67fa5823b8c390fa75de041bb
AlphoAnn <141331765+AlphoAnn@users.noreply.github.com>
2023-09-04 13:57:30 +0530

    Create 3Ddonut1.js

---
+++ 3Ddonut1.js
@@ -0,0 +1,93 @@
+var getScriptPromisify = (src) => {
+    return new Promise(resolve => {
+        $.getScript(src, resolve)
+    })
+}
+
+(function () {
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
+<style>
+</style>
+<div id="root" style="width: 100%; height: 100%;">
+</div>
+`
+    class SamplePrepared extends HTMLElement {
+        constructor() {
+            super()
+
+            this._shadowRoot = this.attachShadow({ mode: 'open' })
+            this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+
+            this._root = this._shadowRoot.getElementById('root')
+
+            this._props = {}
+
+            this.render()
+        }
+
+        onCustomWidgetResize(width, height) {
+            this.render()
+        }
+
+        async render() {
+            await getScriptPromisify('https://cdn.amcharts.com/lib/4/core.js');
+            await getScriptPromisify('https://cdn.amcharts.com/lib/4/themes/animated.js');
+            await getScriptPromisify('https://cdn.amcharts.com/lib/4/charts.js');
+
+  am4core.useTheme(am4themes_animated);
+// Themes end
+
+var chart = am4core.create(this._root, am4charts.PieChart3D);
+chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+chart.legend = new am4charts.Legend();
+
+chart.data = [
+  {
+    country: "Lithuania",
+    litres: 501.9
+  },
+  {
+    country: "Czech Republic",
+    litres: 301.9
+  },
+  {
+    country: "Ireland",
+    litres: 201.1
+  },
+  {
+    country: "Germany",
+    litres: 165.8
+  },
+  {
+    country: "Australia",
+    litres: 139.9
+  },
+  {
+    country: "Austria",
+    litres: 128.3
+  },
+  {
+    country: "UK",
+    litres: 99
+  },
+  {
+    country: "Belgium",
+    litres: 60
+  },
+  {
+    country: "The Netherlands",
+    litres: 50
+  }
+];
+
+chart.innerRadius = 100;
+
+var series = chart.series.push(new am4charts.PieSeries3D());
+series.dataFields.value = "litres";
+series.dataFields.category = "country";
+		}
+	}
+	customElements.define("com-demo-gradientgauge1", SamplePrepared)
+})()
