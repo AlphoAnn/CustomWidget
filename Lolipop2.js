a1534b02ccee73de5d4371862aa4ec0e00a142b1
AlphoAnn <141331765+AlphoAnn@users.noreply.github.com>
2023-09-05 12:55:25 +0530

    Update Lolipop2.js

--- Lolipop2.js
+++ Lolipop2.js
@@ -40,10 +40,10 @@ var getScriptPromisify = (src) => {
 			// create chart
 			var chart = am4core.create(this._root, am4charts.XYChart);
 
-		console.log(resultset1);
+		console.log(["rs",resultset1]);
 		var data = [];
 		
-		for(var i = 0; i<resultset1.length;i++){
+		for(var i = 0; i<resultset1.length; i++){
 			var a = {
 				salesmanager:resultset1[i]["Sales_Manager__5w3m5d06b5"].description,
 				grossmargin:resultset1[i]["@MeasureDimension"].rawvalue
