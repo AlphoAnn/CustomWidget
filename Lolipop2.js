f25714a76c9a4a85fe10cdac3f2b25f3e48f384d
AlphoAnn <141331765+AlphoAnn@users.noreply.github.com>
2023-09-05 12:58:41 +0530

    Update Lolipop2.js

--- Lolipop2.js
+++ Lolipop2.js
@@ -40,7 +40,7 @@ var getScriptPromisify = (src) => {
 			// create chart
 			var chart = am4core.create(this._root, am4charts.XYChart);
 
-		console.log(["rs",resultset1]);
+		console.log(resultset1);
 		var data = [];
 		
 		for(var i = 0; i<resultset1.length; i++){
