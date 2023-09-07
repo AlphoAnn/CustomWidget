047351769c4b00f362ab2ec7e3e216197520cc96
AlphoAnn <141331765+AlphoAnn@users.noreply.github.com>
2023-09-07 12:37:26 +0530

    Update Colchrtaxbrk.js

--- Colchrtaxbrk.js
+++ Colchrtaxbrk.js
@@ -35,36 +35,119 @@ var getScriptPromisify = (src) => {
             await getScriptPromisify('https://cdn.amcharts.com/lib/4/themes/animated.js');
             await getScriptPromisify('https://cdn.amcharts.com/lib/4/charts.js');
 
-            am4core.useTheme(am4themes_animated);
-			
-			// create chart
-			var chart = am4core.create(this._root, am4charts.GaugeChart);
-			chart.innerRadius = -15;
+am4core.useTheme(am4themes_animated);
+// Themes end
 
-			var axis = chart.xAxes.push(new am4charts.ValueAxis());
-			axis.min = 0;
-			axis.max = 100;
-			axis.strictMinMax = true;
+var chart = am4core.create(this._root, am4charts.XYChart);
+chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
-			var colorSet = new am4core.ColorSet();
+chart.data = [
+  {
+    country: "USA",
+    visits: 23725
+  },
+  {
+    country: "China",
+    visits: 1882
+  },
+  {
+    country: "Japan",
+    visits: 1809
+  },
+  {
+    country: "Germany",
+    visits: 1322
+  },
+  {
+    country: "UK",
+    visits: 1122
+  },
+  {
+    country: "France",
+    visits: 1114
+  },
+  {
+    country: "India",
+    visits: 984
+  },
+  {
+    country: "Spain",
+    visits: 711
+  },
+  {
+    country: "Netherlands",
+    visits: 665
+  },
+  {
+    country: "Russia",
+    visits: 580
+  },
+  {
+    country: "South Korea",
+    visits: 443
+  },
+  {
+    country: "Canada",
+    visits: 441
+  }
+];
 
-			var gradient = new am4core.LinearGradient();
-			gradient.stops.push({color:am4core.color("red")})
-			gradient.stops.push({color:am4core.color("yellow")})
-			gradient.stops.push({color:am4core.color("green")})
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.dataFields.category = "country";
+categoryAxis.renderer.minGridDistance = 40;
+categoryAxis.fontSize = 11;
 
-			axis.renderer.line.stroke = gradient;
-			axis.renderer.line.strokeWidth = 15;
-			axis.renderer.line.strokeOpacity = 1;
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.min = 0;
+valueAxis.max = 24000;
+valueAxis.strictMinMax = true;
+valueAxis.renderer.minGridDistance = 30;
+// axis break
+var axisBreak = valueAxis.axisBreaks.create();
+axisBreak.startValue = 2100;
+axisBreak.endValue = 22900;
+//axisBreak.breakSize = 0.005;
 
-			axis.renderer.grid.template.disabled = true;
+// fixed axis break
+var d = (axisBreak.endValue - axisBreak.startValue) / (valueAxis.max - valueAxis.min);
+axisBreak.breakSize = 0.05 * (1 - d) / d; // 0.05 means that the break will take 5% of the total value axis height
 
-			var hand = chart.hands.push(new am4charts.ClockHand());
-			hand.radius = am4core.percent(97);
+// make break expand on hover
+var hoverState = axisBreak.states.create("hover");
+hoverState.properties.breakSize = 1;
+hoverState.properties.opacity = 0.1;
+hoverState.transitionDuration = 1500;
 
-			setInterval(function() {
-				hand.showValue(Math.random() * 100, 1000, am4core.ease.cubicOut);
-			}, 2000);
+axisBreak.defaultState.transitionDuration = 1000;
+/*
+// this is exactly the same, but with events
+axisBreak.events.on("over", function() {
+  axisBreak.animate(
+    [{ property: "breakSize", to: 1 }, { property: "opacity", to: 0.1 }],
+    1500,
+    am4core.ease.sinOut
+  );
+});
+axisBreak.events.on("out", function() {
+  axisBreak.animate(
+    [{ property: "breakSize", to: 0.005 }, { property: "opacity", to: 1 }],
+    1000,
+    am4core.ease.quadOut
+  );
+});*/
+
+var series = chart.series.push(new am4charts.ColumnSeries());
+series.dataFields.categoryX = "country";
+series.dataFields.valueY = "visits";
+series.columns.template.tooltipText = "{valueY.value}";
+series.columns.template.tooltipY = 0;
+series.columns.template.strokeOpacity = 0;
+
+// as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+series.columns.template.adapter.add("fill", function(fill, target) {
+  return chart.colors.getIndex(target.dataItem.index);
+});
 		}
 	}
 	customElements.define("com-demo-colchrtaxbrk1", SamplePrepared)
